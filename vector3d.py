f96e80548eda6523e3acb0f4f6bbe969b0c14cba
Sebastian Plamauer <oeplse@gmail.com>
2015-06-17 19:44:11 +0200

    pep8 mostly clean

--- vector3d.py
+++ vector3d.py
@@ -26,8 +26,13 @@ THE SOFTWARE.
 import pyb
 from math import sqrt, degrees, acos, atan2
 
+
 def default_wait():
+    '''
+    delay of 50 ms
+    '''
     pyb.delay(50)
+
 
 class Vector3d(object):
     '''
@@ -36,9 +41,9 @@ class Vector3d(object):
     Returns vehicle-relative x, y and z values.
     '''
     def __init__(self, transposition, scaling, update_function):
-        self._vector = [0,0,0]
-        self._ivector = [0,0,0]
-        self.cal = (0,0,0)
+        self._vector = [0, 0, 0]
+        self._ivector = [0, 0, 0]
+        self.cal = (0, 0, 0)
         self.argcheck(transposition, "Transposition")
         self.argcheck(scaling, "Scaling")
         if (len(transposition) != len(set(transposition))) or min(transposition) < 0 or max(transposition) > 2:
@@ -48,23 +53,32 @@ class Vector3d(object):
         self.update = update_function
 
     def argcheck(self, arg, name):
+        '''
+        checks if arguments are of correct length
+        '''
         if len(arg) != 3 or not (type(arg) is list or type(arg) is tuple):
             raise ValueError(name + ' must be a 3 element list or tuple')
 
-    def calibrate(self, stopfunc, waitfunc = default_wait):
+    def calibrate(self, stopfunc, waitfunc=default_wait):
+        '''
+        calibration routine, sets cal
+        '''
         self.update()
         maxvec = self._vector[:]                # Initialise max and min lists with current values
-        minvec =self._vector[:]
+        minvec = self._vector[:]
         while not stopfunc():
             waitfunc()
             self.update()
             maxvec = list(map(max, maxvec, self._vector))
             minvec = list(map(min, minvec, self._vector))
-        self.cal = tuple(map(lambda a, b: (a +b)/2, maxvec, minvec))
+        self.cal = tuple(map(lambda a, b: (a + b)/2, maxvec, minvec))
 
     @property
-    def _calvector(self):                       # Vector adjusted for calibration offsets
-        return list(map(lambda val, offset : val - offset, self._vector, self.cal))
+    def _calvector(self):
+        '''
+        Vector adjusted for calibration offsets
+        '''
+        return list(map(lambda val, offset: val - offset, self._vector, self.cal))
 
     @property
     def x(self):                                # Corrected, vehicle relative floating point values
@@ -90,12 +104,8 @@ class Vector3d(object):
 
     @property
     def magnitude(self):
-        x, y, z = self.xyz # All measurements must correspond to the same instant
+        x, y, z = self.xyz  # All measurements must correspond to the same instant
         return sqrt(x**2 + y**2 + z**2)
-
-    @property
-    def elevation(self):
-        return 90 - self.inclination
 
     @property
     def inclination(self):
@@ -127,10 +137,11 @@ class Vector3d(object):
     @property
     def ixyz(self):
         return self._ivector
+
     @property
     def transpose(self):
         return tuple(self._transpose)
+
     @property
     def scale(self):
         return tuple(self._scale)
-
